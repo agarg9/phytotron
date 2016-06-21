8c59c2aac34b0d809068f1070b3db2f9f4ded35e
agarg9 <agarg9@ncsu.edu>
2016-06-21 18:02:10 -0400

    started integrating with angular,user partials

--- js/app.js
+++ js/app.js
@@ -1,7 +1,72 @@
-(function(){
-    var app = angular.module('phytotron', []);
-    app.controller('PhytotronController',function(){
-        this.product=gem;
+/*(function () {
+    var app = angular.module("phytotron", [ ]);
+    app.controller('phyController',function() {
+        this.project = project;
     });
-    
-}();
+    var project = {
+        name: 'abcd',
+        price:3.44
+    }
+})();
+      */
+
+(function () {
+var app = angular.module("phytotron", ["ngRoute"]);
+    app.config(['$routeProvider',function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "partials/home.html",
+                controller: "abcController"
+            })
+            .when("/home", {
+                templateUrl: "partials/home.html",
+                controller: "homeController"
+            })
+            .when("/history", {
+                templateUrl: "partials/history.html",
+                controller: "historyController"
+            })
+            .when("/newAppl", {
+                templateUrl: "partials/newAppl.html",
+                controller: "newApplController"
+            })
+            .when("/chambers", {
+                templateUrl: "partials/sample.html",
+                controller: "sampleController"
+            })
+    }]);
+   /* .controller("abcController", function ($scope) {
+        $scope.message = "1234";
+    })
+    .controller("homeController", function ($scope) {
+        $scope.message = "xyz";
+    })
+    .controller("sampleController", function ($scope) {})
+});*/
+
+})();
+
+/*var app = angular.module("phytotron", ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "partials/home.html",
+                controller: "abcController"
+            })
+            .when("/home", {
+                templateUrl: "partials/home.html",
+                controller: "homeController"
+            })
+            .when("/sample", {
+                templateUrl: "partials/sample.html",
+                controller: "sampleController"
+            })
+    })
+    .controller("abcController", function ($scope) {
+        $scope.message = "1234";
+    })
+   .controller("homeController", function ($scope) {
+        $scope.message = "xyz";
+    })
+    .controller("sampleController", function ($scope) {})
+});*/
